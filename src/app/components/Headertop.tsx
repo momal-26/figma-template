da4b51a12c149c46a1815f03e4f9ac90bf2e747e
momal-26 <85959276+momal-26@users.noreply.github.com>
2024-12-06 02:17:56 +0500

    Update Headertop.tsx

--- src/app/components/Headertop.tsx
+++ src/app/components/Headertop.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
  export default function Headertop(){
     return(
 
-<div className="text-white  text-[15px] sm:w-full md:w-[80px] text-left md:px-18 sm:px-20 bg-blue-900">
+<div className="text-white  text-[15px] sm:w-full md:w-[80px] text-left md:px-25 sm:px-20 bg-blue-900">
                  free shipping on all orders over $50 
 
 <div className="flex justify-end items-center sm:w-[80px] md:w-full pr-8 ">
             <div className="flex space-x-6">
                <Image src="/images/ENG.png" alt="image3" width={30} height={30}/>
-               <Image src="/images/Faqs.png" alt="image2" width={30} height={30}/>
+               <Image src="/images/Faqs.png" alt="image2" width={30} height={10}/>
                <Image src="/images/need.png" alt="image1" width={50} height={20}/>
             </div>
  </div>
