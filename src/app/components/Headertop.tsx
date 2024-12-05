adedfee110c92fd2b9a109399681f2df732c12d3
momal-26 <85959276+momal-26@users.noreply.github.com>
2024-12-06 02:28:36 +0500

    Update Headertop.tsx

--- src/app/components/Headertop.tsx
+++ src/app/components/Headertop.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image';
 <div className="flex justify-end items-center sm:w-[80px] md:w-full pr-8 ">
             <div className="flex space-x-6">
                <Image src="/images/ENG.png" alt="image3" width={30} height={30}/>
-               <Image src="/images/Faqs.png" alt="image2" width={30} height={10}/>
+               <Image src="/images/Faqs.png" alt="image2" width={30} height={50}/>
                <Image src="/images/need.png" alt="image1" width={50} height={20}/>
             </div>
  </div>
